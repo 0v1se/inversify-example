6be06b039960291e40337a918d1d1f6d9683cc64
0v1se <dev@0v1se.com>
2019-09-23 20:34:23 +0300

    async container modules

--- src/lib/container.spec.ts
+++ src/lib/container.spec.ts
@@ -1,6 +1,6 @@
 // tslint:disable:no-expression-statement
 import test from 'ava';
-import { Container } from 'inversify';
+import { AsyncContainerModule, Container } from 'inversify';
 import { buildProviderModule } from 'inversify-binding-decorators';
 import 'reflect-metadata';
 import { ComplexComponent, Handler, SimpleComponent } from './container';
@@ -38,3 +38,24 @@ test('should use singletons', t => {
   const bean2 = c.get<SimpleComponent>(SimpleComponent);
   t.true(bean === bean2);
 });
+
+test('should use modules', async t => {
+  const c = new Container();
+  c.options.defaultScope = 'Singleton';
+  c.load(buildProviderModule());
+
+  const module = new AsyncContainerModule(async (bind) => {
+    const value = await Promise.resolve("testValue");
+    bind("value").toConstantValue(value);
+  });
+
+  await c.loadAsync(module);
+
+  const bean = c.get(SimpleComponent);
+  const bean2 = c.get(SimpleComponent);
+  t.true(bean === bean2);
+  const cc = c.get(ComplexComponent);
+  t.true(cc != null);
+});
+
+
